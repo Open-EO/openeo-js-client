dc590dc97ec4c41ad4742decf0a31818aa6f182c
Matthias Mohr <m.mohr@uni-muenster.de>
2018-02-21 18:35:11 +0100

    Migrated to API version 0.0.2 and added new functions (first draft).

--- openeo.js
+++ openeo.js
@@ -1,44 +1,83 @@
-class ImageCollectionNode {
+//// PROCESS GRAPH CREATION ////
+class ProcessGraphNode {
 	constructor() { }
 
-	filter_daterange(startT, endT) {
+	filter_daterange(startT, endT, imagery = null) {
 		return new ProcessNode('filter_daterange', {
-			collections: [this],
+			imagery: (imagery ? imagery : this),
 			from: startT,
 			to: endT
 		});
 	}
 
-	filter_bbox(box, crs = 'EPSG:4326') {
+	filter_bbox(left, top, right, bottom, crs = 'EPSG:4326', imagery = null) {
+		imagery = imagery ? imagery : this;
 		return new ProcessNode('filter_bbox', {
-			collections: [this],
+			imagery: (imagery ? imagery : this),
 			srs: crs,
-			bbox: box
+			left: left,
+			right: right,
+			top: top,
+			bottom: bottom
 		});
 	}
 
-	NDI(first, second) {
-		return new ProcessNode('NDI', {
-			collections: [this],
+	filter_bands(bands, imagery = null) {
+		return new ProcessNode('filter_bands', {
+			imagery: (imagery ? imagery : this),
+			bands: bands
+		});
+		// ToDo: add band names and wavelength filters
+	}
+
+	NDVI(first, second, imagery = null) {
+		return new ProcessNode('NDVI', {
+			imagery: (imagery ? imagery : this),
 			band1: first,
 			band2: second
 		});
 	}
 
-	min_time() {
+	min_time(imagery = null) {
 		return new ProcessNode('min_time', {
-			collections: [this]
+			imagery: (imagery ? imagery : this)
 		});
 	}
 
-	max_time() {
+	max_time(imagery = null) {
 		return new ProcessNode('max_time', {
-			collections: [this]
+			imagery: (imagery ? imagery : this)
 		});
 	}
+	
+	udf(language, process_id, script, imagery = null) {
+		return new UdfProcessNode(language, process_id, script, imagery);
+	}
+	
+	zonal_statistics(regionsPath, func, imagery = null) {
+		return new ProcessNode('zonal_statistics', {
+			imagery: (imagery ? imagery : this),
+			regions: regionsPath,
+			func: func
+		});
+	}
+	
+	custom(process_id, args, processParameterName, imagery = null) {
+		// ToDo: Improve? Doesn't seem very tidy (changing an object from outer scope).
+		args[processParameterName] = (imagery ? imagery : this);
+		return new ProcessNode(process_id, args);
+	}
+	
+	execute(output_args = {}) {
+		return OpenEO.HTTP.post('/execute', {
+			process_graph: this, 
+			output: output_args
+		});
+	}
+	
 }
 
-class ProcessNode extends ImageCollectionNode {
+class ProcessNode extends ProcessGraphNode {
 	constructor(process_id, args) {
 		super();
 		this.process_id = process_id;
@@ -46,22 +85,160 @@ class ProcessNode extends ImageCollectionNode {
 	}
 }
 
-class SourceDatasetNode extends ImageCollectionNode {
+class UdfProcessNode extends ProcessNode {
+	constructor(language, process_id, script, imagery = null) {
+		super('udf/' + language + '/' + process_id, {
+			imagery: (imagery ? imagery : this),
+			script: script
+		});
+	}
+	
+}
+
+class ImageCollectionNode extends ProcessGraphNode {
 	constructor(srcId) {
 		super();
 		this.product_id = srcId;
 	}
 }
 
+//// API SUB-COMPONENTS ////
+class UserAPI {
+	
+	constructor(user_id) {
+		this.user_id = user_id;
+	}
+	
+	getProcessGraphs() {
+		throw new Exception('Not implemented');
+	}
+	
+	createProcessGraph() {
+		throw new Exception('Not implemented');
+	}
+	
+	getProcessGraphObject(process_graph_id) {
+		return new UserProcessGraphAPI(this.user_id, process_graph_id);
+	}
+	
+	getFiles() {
+		throw new Exception('Not implemented');
+	}
+	
+	getFileObject(path) {
+		return new UserFileAPI(this.user_id, path);
+	}
+	
+	getJobs() {
+		throw new Exception('Not implemented');
+	}
+	
+	getServices() {
+		throw new Exception('Not implemented');
+	}
+	
+	getCredits() {
+		throw new Exception('Not implemented');
+	}
+	
+}
+
+class UserProcessGraphAPI {
+	
+	constructor(user_id, process_graph_id) {
+		this.user_id = user_id;
+		this.process_graph_id = process_graph_id;
+	}
+	
+	get() {
+		throw new Exception('Not implemented');
+	}
+	
+	replace() {
+		throw new Exception('Not implemented');
+	}
+	
+	delete() {
+		throw new Exception('Not implemented');
+	}
+	
+}
+
+class UserFileAPI {
+	
+	constructor(user_id, path) {
+		this.user_id = user_id;
+		this.path = path;
+	}
+	
+	get() {
+		throw new Exception('Not implemented');
+	}
+	
+	replace() {
+		throw new Exception('Not implemented');
+	}
+	
+	delete() {
+		throw new Exception('Not implemented');
+	}
+	
+}
+
+class JobAPI {
+	
+	constructor(job_id) {
+		this.job_id = job_id;
+	}
+	
+	modify() {
+		throw new Exception('Not implemented');
+	}
+	
+	get() {
+		throw new Exception('Not implemented');
+	}
+	
+	subscribe() {
+		throw new Exception('Not implemented');
+	}
+	
+	queue() {
+		throw new Exception('Not implemented');
+	}
+	
+	pause() {
+		throw new Exception('Not implemented');
+	}
+	
+	cancel() {
+		throw new Exception('Not implemented');
+	}
+	
+}
+
+class ServiceAPI {
+	
+	constructor(service_id) {
+		this.service_id = service_id;
+	}
+	
+	modify() {
+		throw new Exception('Not implemented');
+	}
+	
+	get() {
+		throw new Exception('Not implemented');
+	}
+	
+	delete() {
+		throw new Exception('Not implemented');
+	}
+	
+}
+
+//// API ////
 var OpenEO = {
-
-	ImageCollection: {
-
-		create: function (collId) {
-			return new SourceDatasetNode(collId);
-		}
-
-	},
 
 	API: {
 
@@ -71,8 +248,20 @@ var OpenEO = {
 		// Currently this is only to work around specific behaviour of backends
 		// during development phase.
 		driver: null,
+	
+		getCapabilities() {
+			return OpenEO.HTTP.get('/capabilities');
+		},
+		
+		getOutputFormats() {
+			return OpenEO.HTTP.get('/capabilities/output_formats');
+		}
 
-		get: function (path, query, responseType) {
+	},
+	
+	HTTP: {
+
+		get(path, query, responseType) {
 			return this.send({
 				method: 'get',
 				responseType: responseType,
@@ -81,7 +270,7 @@ var OpenEO = {
 			});
 		},
 
-		post: function (path, body, responseType) {
+		post(path, body, responseType) {
 			var options = {
 				method: 'post',
 				responseType: responseType,
@@ -98,14 +287,14 @@ var OpenEO = {
 			return this.send(options);
 		},
 
-		send: function (options) {
-			options.baseURL = this.baseUrl;
-			if (OpenEO.Auth.hasCredentials()) {
+		send(options) {
+			options.baseURL = OpenEO.API.baseUrl;
+			if (OpenEO.Auth.isLoggedIn()) {
 				options.withCredentials = true;
-				options.auth = {
-					username: OpenEO.Auth.username,
-					password: OpenEO.Auth.password
-				};
+				if (!options.headers) {
+					options.headers = {};
+				}
+				options.headers['Authentication'] = 'Bearer ' + OpenEO.Auth.token;
 			}
 			if (!options.responseType) {
 				options.responseType = 'json';
@@ -117,47 +306,51 @@ var OpenEO = {
 
 	},
 
-	Jobs: {
+	ImageCollection: {
 
-		// ToDo: This should be temporary and be replaced with a more elegant solution
-		// see: /jobs/{job_id}/download and services in API 0.0.2
-		getWcsPath: function (jobId) {
-			return OpenEO.API.baseUrl + '/download/' + jobId + '/wcs';
-		},
-
-		create: function (processGraph) {
-			return OpenEO.API.post('/jobs', {
-				process_graph: processGraph
-			});
+		create(collId) {
+			return new ImageCollectionNode(collId);
 		}
 
 	},
 
 	Auth: {
 
-		username: null,
-		password: null,
 		userId: null,
+		token: null,
 
-		hasCredentials: function () {
-			return Boolean(this.username && this.password);
+		isLoggedIn() {
+			return (this.token !== null);
 		},
 
-		setCredentials: function (username, password) {
-			this.username = username;
-			this.password = password;
+		login(username, password) {
+			var options = {
+				method: 'get',
+				url: '/auth/login',
+				withCredentials: true,
+				auth: {
+					username: username,
+					password: password
+				}
+			};
+			return OpenEO.HTTP.send(options).then(data => {
+				this.userId = data.user_id;
+				this.token = data.token;
+			}).catch(code => {
+				if (code == 403) {
+					this.userId = null;
+					this.token = null;
+				}
+			});
 		},
 
-		login: function () {
-			return OpenEO.API.get('/auth/login');
-		}
-
-	},
-
-	Capabilities: {
-
-		get: function () {
-			return OpenEO.API.get('/capabilities');
+		register(password) {
+			var body = {
+				password: password
+			};
+			return OpenEO.HTTP.post('/auth/register', body).then(data => {
+				this.userId = data.user_id;
+			});
 		}
 
 	},
@@ -171,29 +364,25 @@ var OpenEO = {
 			qenddate: null
 		},
 
-		OpenSearchOptions: {
-			// ToDo
-		},
-
-		get: function (options) {
+		get(options) {
 			var opts = options || this.DefaultQueryOptions;
 			var path = '/data';
 			// ToDo: Remove this, it's just for the R backend for now
 			if (OpenEO.API.driver === 'openeo-r-backend') {
 				path += '/';
 			}
-			return OpenEO.API.get(path, opts);
+			return OpenEO.HTTP.get(path, opts);
 		},
 
-		getById: function (id) {
-			return OpenEO.API.get('/data/' + id);
+		getById(id) {
+			return OpenEO.HTTP.get('/data/' + id);
 		}
 
 	},
 
 	Processes: {
 
-		get: function (name) {
+		get(name) {
 			var query = {};
 			if (name) {
 				query.qname = name;
@@ -203,13 +392,63 @@ var OpenEO = {
 			if (OpenEO.API.driver === 'openeo-r-backend') {
 				path += '/';
 			}
-			return OpenEO.API.get(path, query);
+			return OpenEO.HTTP.get(path, query);
 		},
 
-		getById: function (id) {
-			return OpenEO.API.get('/processes/' + id);
+		getById(id) {
+			return OpenEO.HTTP.get('/processes/' + id);
 		}
 
+	},
+	
+	UDFRuntimes: {
+		
+		get() {
+			throw new Exception('Not implemented');
+		},
+		
+		getProcess(lang, udf_type) {
+			throw new Exception('Not implemented');
+		}
+		
+	},
+
+	Jobs: {
+
+		create(processGraph) {
+			return OpenEO.API.post('/jobs', {
+				process_graph: processGraph
+			});
+		},
+		
+		getObject(job_id) {
+			return new JobAPI(job_id);
+		}
+
+	},
+	
+	Services: {
+		
+		create() {
+			throw new Exception('Not implemented');
+		},
+		
+		getCapabilities() {
+			return OpenEO.HTTP.get('/capabilities/services');
+		},
+		
+		getObject(service_id) {
+			return new ServiceAPI(service_id);
+		}
+		
+	},
+	
+	Users: {
+		
+		getObject(user_id) {
+			return new UserAPI(user_id);
+		}
+		
 	}
 
 };
