8233bdd3c1eb2f73d1ee170c9bc90d3ccbca4a31
Christoph Friedrich <christoph@friedrich-whv.de>
2018-11-19 22:05:44 +0100

    Add extensive CRUD tests for process graphs
    List the emptiness, create one, list again and verify it now exists, add another, verify the two, describe one of them, describe the other, update one, delete the other.

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -16,6 +16,7 @@ describe('With earth-engine-driver', () => {
 	const TESTCAPABILITIES = JSON.parse('{"version":"0.3.1","endpoints":[{"path":"/","methods":["GET"]},{"path":"/service_types","methods":["GET"]},{"path":"/output_formats","methods":["GET"]},{"path":"/stac","methods":["GET"]},{"path":"/collections","methods":["GET"]},{"path":"/collections/{collection_id}","methods":["GET"]},{"path":"/processes","methods":["GET"]},{"path":"/files/{user_id}","methods":["GET"]},{"path":"/files/{user_id}/{path}","methods":["GET","PUT","DELETE"]},{"path":"/preview","methods":["POST"]},{"path":"/jobs","methods":["POST","GET"]},{"path":"/jobs/{job_id}","methods":["GET","PATCH","DELETE"]},{"path":"/jobs/{job_id}/results","methods":["GET","POST","DELETE"]},{"path":"/temp/{token}/{file}","methods":["GET"]},{"path":"/services","methods":["GET","POST"]},{"path":"/services/{service_id}","methods":["GET","PATCH","DELETE"]},{"path":"/xyz/{service_id}/{z}/{x}/{y}","methods":["GET"]},{"path":"/subscription","methods":["GET"]},{"path":"/credentials/basic","methods":["GET"]},{"path":"/credentials","methods":["POST"]},{"path":"/me","methods":["GET"]},{"path":"/validation","methods":["POST"]},{"path":"/process_graphs","methods":["GET","POST"]},{"path":"/process_graphs/{process_graph_id}","methods":["GET","PATCH","DELETE"]}],"billing":{"currency":"USD","default_plan":"free","plans":[{"name":"free","description":"Earth Engine is free for research, education, and nonprofit use. For commercial applications, Google offers paid commercial licenses. Please contact earthengine-commercial@google.com for details."}]}}');
 	const TESTCOLLECTION = JSON.parse(`{"name":"USGS/GTOPO30","title":"GTOPO30: Global 30 Arc-Second Elevation","description":"GTOPO30 is a global digital elevation model (DEM) with a horizontal grid spacing of 30 arc seconds (approximately 1 kilometer). The DEM was derived from several raster and vector sources of topographic information.  Completed in late 1996, GTOPO30 was developed over a three-year period through a collaborative effort led by the U.S. Geological Survey's Center for Earth Resources Observation and Science (EROS). The following organizations  participated by contributing funding or source data:  the National Aeronautics  and Space Administration (NASA), the United Nations Environment Programme/Global Resource Information Database (UNEP/GRID), the U.S. Agency for International Development (USAID), the Instituto Nacional de Estadistica Geografica e Informatica (INEGI) of Mexico, the Geographical Survey Institute  (GSI) of Japan, Manaaki Whenua Landcare Research of New Zealand, and the  Scientific Committee on Antarctic Research (SCAR).","license":"proprietary","extent":{"spatial":[-180,-90,180,90],"temporal":["1996-01-01T00:00:00Z","1996-01-01T00:00:00Z"]},"links":[{"rel":"self","href":"https://earthengine.openeo.org/v0.3/collections/USGS/GTOPO30"},{"rel":"parent","href":"https://earthengine.openeo.org/v0.3/collections"},{"rel":"root","href":"https://earthengine.openeo.org/v0.3/collections"}]}`);
 	const TESTPROCESS = JSON.parse('{"name":"count_time","description":"Counts the number of images with a valid mask in a time series for all bands of the input dataset.","parameters":{"imagery":{"description":"EO data to process.","required":true,"schema":{"type":"object","format":"eodata"}}},"returns":{"description":"Processed EO data.","schema":{"type":"object","format":"eodata"}}}');
+	const TESTPROCESSGGRAPH = {"process_id": "stretch_colors","imagery": {"process_id": "min_time","imagery": {"process_id": "NDVI","imagery": {"process_id": "filter_daterange","imagery": {"process_id": "get_collection","name": "COPERNICUS/S2"},"extent": ["2018-01-01T00:00:00Z","2018-01-31T23:59:59Z"]},"red": "B4","nir": "B8"}},"min": -1,"max": 1};
 	var obj = new OpenEO();
 
 	async function connectWithoutAuth() {
@@ -178,5 +179,125 @@ describe('With earth-engine-driver', () => {
 			expect(fs).toHaveLength(0);
 		});
 	});
-});
 
+	describe('CRUD process graphs', async () => {
+		var con;
+		beforeAll(async (done) => {
+			con = await connectWithBasicAuth();
+			done();
+		});
+
+		test('List process graphs', async () => {
+			var pgs = await con.listProcessGraphs();
+			expect(pgs).not.toBeNull();
+		});
+
+		test('Make sure there are no process graphs', async () => {
+			var pgs = await con.listProcessGraphs();
+			expect(pgs).toHaveLength(0);
+		});
+
+		var pg1;
+		var pg2;
+		test('Add process graph without metadata', async () => {
+			var pg = await con.createProcessGraph(TESTPROCESSGGRAPH);
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(Object.getPrototypeOf(pg).constructor.name).toBe('ProcessGraph');
+			expect(pg.connection).toBe(con);
+			expect(pg.processGraphId).not.toBeNull();
+			expect(pg.processGraphId).not.toBeUndefined();
+			expect(pg.title).toBeNull();
+			expect(pg.description).toBeNull();
+			pg1 = pg;
+		});
+
+		test('Make sure there is now 1 process graph, with no metadata', async () => {
+			var pgs = await con.listProcessGraphs();
+			expect(pgs).toHaveLength(1);
+			expect(pgs[0]).not.toBeNull();
+			expect(pgs[0]).not.toBeUndefined();
+			expect(Object.getPrototypeOf(pgs[0]).constructor.name).toBe('ProcessGraph');
+			expect(pgs[0].processGraphId).not.toBeNull();
+			expect(pgs[0].processGraphId).toBe(pg1.processGraphId);
+			expect(pgs[0].title).toBeNull();
+			expect(pgs[0].description).toBeNull();
+			expect(pgs[0]).toEqual(pg1);
+		});
+
+		test('Add process graph with metadata', async () => {
+			var pg = await con.createProcessGraph(TESTPROCESSGGRAPH, 'Test title', 'Test description');
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(Object.getPrototypeOf(pg).constructor.name).toBe('ProcessGraph');
+			expect(pg.connection).toBe(con);
+			expect(pg.processGraphId).not.toBeNull();
+			expect(pg.processGraphId).not.toBe(pg1.processGraphId);
+			expect(pg.title).toBe('Test title');
+			expect(pg.description).toBe('Test description');
+			pg2 = pg;
+		});
+
+		test('Make sure there are now 2 process graphs, the second with metadata', async () => {
+			var pgs = await con.listProcessGraphs();
+			expect(pgs).toHaveLength(2);
+			var pg = (pgs[0].processGraphId != pg1.processGraphId ? pgs[0] : pgs[1]);
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(Object.getPrototypeOf(pg).constructor.name).toBe('ProcessGraph');
+			expect(pg.processGraphId).not.toBeNull();
+			expect(pg.processGraphId).not.toBe(pg1.processGraphId);
+			expect(pg.processGraphId).toBe(pg2.processGraphId);
+			expect(pg.title).toBe('Test title')
+			expect(pg.description).toBe('Test description');
+			if(pgs[0].processGraphId == pg1.processGraphId) {
+				expect(pgs[0]).toEqual(pg1);
+				expect(pgs[1]).toEqual(pg2);
+			} else {
+				expect(pgs[0]).toEqual(pg2);
+				expect(pgs[1]).toEqual(pg1);
+			}
+			
+		});
+
+		test('Describe process graph without metadata', async () => {
+			var pg = await pg1.describeProcessGraph();
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(pg.process_graph_id).not.toBeNull();
+			expect(pg.process_graph_id).toBe(pg1.processGraphId);
+			expect(pg.title).toBeNull();
+			expect(pg.description).toBeNull();
+		});
+
+		test('Describe process graph with metadata', async () => {
+			var pg = await pg2.describeProcessGraph();
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(pg.process_graph_id).not.toBeNull();
+			expect(pg.process_graph_id).toBe(pg2.processGraphId);
+			expect(pg.title).toBe('Test title');
+			expect(pg.description).toBe('Test description');
+		});
+
+		test('Update process graph', async () => {
+			await pg1.updateProcessGraph({title: 'Test title 2'});
+			var pg = await pg1.describeProcessGraph();
+			expect(pg).not.toBeNull();
+			expect(pg).not.toBeUndefined();
+			expect(pg.process_graph_id).not.toBeNull();
+			expect(pg.process_graph_id).toBe(pg1.processGraphId);
+			expect(pg.title).toBe('Test title 2');
+			expect(pg.description).toBeNull();
+		});
+
+		test('Delete the second process graph', async () => {
+			const success = await pg2.deleteProcessGraph();
+			expect(success).toBeTruthy();
+			
+			var pgs = await con.listProcessGraphs();
+			expect(pgs).toHaveLength(1);
+			expect(pgs[0].processGraphId).toBe(pg1.processGraphId);
+		});
+	});
+});
